2639844101b3c6f3ca469b997ba3a557fdd18650
Seong Eun Kim <ra177143@tolkien.lab.ic.unicamp.br>
2017-08-11 11:27:34 -0300

    atualizando lista ligada

--- listaLigada.c
+++ listaLigada.c
@@ -29,8 +29,34 @@ void leLista(itemPatrimonio* noCabeca, FILE *arqsaida){
     fprintf(arqsaida, "valor: %d\n", noCabeca->valor);
     fprintf(arqsaida, "data de aquisicao: %s\n", noCabeca->dataAquisicao);
     
+    historico* node = noCabeca->meuhistorico;
+    
+    while(meuhistorico->next != NULL){ 
+        fprinf(arqsaida, "estava em %s na data %s\n", meuhistorico->local, meuhistorico->data); 
+        meuhistorico = meuhistorico->next;
+    }
+    
     if(noCabeca->next == NULL)
         return;
     
     leLista(noCabeca->next);
+}
+
+void addHistorico(itemPatrimonio* noCabeca, int numPatrimonio, char data[], char local[]){
+    while(noCabeca->next != NULL && noCabeca->numPatrimonio != numPatrimonio)
+        noCabeca = noCabeca->next;
+    
+    if(noCabeca->numPatrimonio != numPatrimonio)
+        return;
+    
+    historico* node = noCabeca->meuhistorico;
+    
+    while(meuhistorico->next != NULL) meuhistorico = meuhistorico->next;
+    
+    historico* newNode = malloc(sizeof(historico));
+    strcpy(newNode->data, data);
+    strcpy(newNode->local, local);
+    newNode->next = NULL;
+    meuhistorico->next = newNode;    
+    
 }
--- listaLigada.h
+++ listaLigada.h
@@ -2,8 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Historico{
+    char local[100];
+    char data[20];
+    struct Historico* next;
+} historico;
+
+typedef struct ItemPatrimonio{
+    int numPatrimonio;
+    char nome[50];
+    int valor;
+    char dataAquisicao[20];
+    struct ItemPatrimonio* next;
+    historico *meuHistorico;
+} itemPatrimonio;
+
 itemPatrimonio novoNo(int numPatrimonio, char nome[], int valor, char dataAquisicao[]);
 
 void addLista(itemPatrimonio* noCabeca,int numPatrimonio, char nome[], int valor, char dataAquisicao[]);
+
+void addHistorico(itemPatrimonio* noCabeca, int numPatrimonio, char data[], char local[]);
 
 void leLista(itemPatrimonio* noCabeca, FILE *arqsaida);
