66ba5c59fe3486a6a56409456a309b8d0a2652ef
Seong Eun Kim <grah.kim@gmail.com>
2017-10-20 15:16:57 -0200

    consultas feitas. correcoes no modelo.

--- proj1/Modelo_fisico_automatico.sql
+++ proj1/Modelo_fisico_automatico.sql
@@ -2,11 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+CREATE SCHEMA IF NOT EXISTS `db_alimentos` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `db_alimentos` ;
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Fabricante`
+-- Table `db_alimentos`.`Fabricante`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Fabricante` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Fabricante` (
   `idFabricante` INT NOT NULL ,
   `Nome` VARCHAR(45) NULL ,
   PRIMARY KEY (`idFabricante`) )
@@ -14,33 +16,33 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Produto`
+-- Table `db_alimentos`.`Produto`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Produto` (
   `Cod_barras` VARCHAR(12) NOT NULL ,
   `Nome` VARCHAR(45) NULL ,
   `idFabricante` INT NOT NULL ,
-  `Calorias` VARCHAR(45) NULL ,
-  `Sódio` VARCHAR(45) NULL ,
-  `Fibras alimentares` VARCHAR(45) NULL ,
-  `Acúcares` VARCHAR(45) NULL ,
-  `Proteínas` VARCHAR(45) NULL ,
-  `Gorduras Totais` VARCHAR(45) NULL ,
-  `Quantidade` VARCHAR(45) NULL ,
+  `Calorias` INT NULL ,
+  `Sodio` INT NULL ,
+  `Fibras alimentares` INT NULL ,
+  `Acucares` INT NULL ,
+  `Proteinas` INT NULL ,
+  `Gorduras Totais` INT NULL ,
+  `Quantidade` INT NULL ,
   PRIMARY KEY (`Cod_barras`) ,
   INDEX `fk_Produto_Fabricante1_idx` (`idFabricante` ASC) ,
   CONSTRAINT `fk_Produto_Fabricante1`
     FOREIGN KEY (`idFabricante` )
-    REFERENCES `db_ra165684`.`Fabricante` (`idFabricante` )
+    REFERENCES `db_alimentos`.`Fabricante` (`idFabricante` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Categoria`
+-- Table `db_alimentos`.`Categoria`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Categoria` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Categoria` (
   `idCategoria` INT NOT NULL ,
   `Nome_Categoria` VARCHAR(45) NULL ,
   PRIMARY KEY (`idCategoria`) )
@@ -48,29 +50,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Ingrediente`
+-- Table `db_alimentos`.`Ingrediente`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Ingrediente` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Ingrediente` (
   `idIngrediente` INT NOT NULL ,
   `Nome` VARCHAR(45) NULL ,
-  `Alérgeno` TINYINT(1) NULL ,
+  `Alergeno` TINYINT(1) NULL ,
   PRIMARY KEY (`idIngrediente`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Pais`
+-- Table `db_alimentos`.`Pais`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Pais` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Pais` (
   `nomePais` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`nomePais`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Paises_Vendidos`
+-- Table `db_alimentos`.`Paises_Vendidos`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Paises_Vendidos` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Paises_Vendidos` (
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   `Pais_nomePais` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`Produto_Cod_barras`, `Pais_nomePais`) ,
@@ -78,21 +80,21 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Paises_Vendidos` (
   INDEX `fk_Produto_has_País onde se vende_Produto1_idx` (`Produto_Cod_barras` ASC) ,
   CONSTRAINT `fk_Produto_has_País onde se vende_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Produto_has_País onde se vende_País onde se vende1`
     FOREIGN KEY (`Pais_nomePais` )
-    REFERENCES `db_ra165684`.`Pais` (`nomePais` )
+    REFERENCES `db_alimentos`.`Pais` (`nomePais` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Produto_has_Ingrediente`
+-- Table `db_alimentos`.`Produto_has_Ingrediente`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Ingrediente` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Produto_has_Ingrediente` (
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   `Ingrediente_idIngrediente` INT NOT NULL ,
   `Porcentagem` INT NULL ,
@@ -101,21 +103,21 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Ingrediente` (
   INDEX `fk_Produto_has_Ingrediente_Produto1_idx` (`Produto_Cod_barras` ASC) ,
   CONSTRAINT `fk_Produto_has_Ingrediente_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Produto_has_Ingrediente_Ingrediente1`
     FOREIGN KEY (`Ingrediente_idIngrediente` )
-    REFERENCES `db_ra165684`.`Ingrediente` (`idIngrediente` )
+    REFERENCES `db_alimentos`.`Ingrediente` (`idIngrediente` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Paises_Fabricados`
+-- Table `db_alimentos`.`Paises_Fabricados`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Paises_Fabricados` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Paises_Fabricados` (
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   `Pais_nomePais` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`Produto_Cod_barras`, `Pais_nomePais`) ,
@@ -123,30 +125,30 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Paises_Fabricados` (
   INDEX `fk_Pais_fabricado_has_Produto_Pais_vendido1_idx` (`Pais_nomePais` ASC) ,
   CONSTRAINT `fk_Local_fabricado_has_Produto_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Pais_fabricado_has_Produto_Pais_vendido1`
     FOREIGN KEY (`Pais_nomePais` )
-    REFERENCES `db_ra165684`.`Pais` (`nomePais` )
+    REFERENCES `db_alimentos`.`Pais` (`nomePais` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Embalagem`
+-- Table `db_alimentos`.`Embalagem`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Embalagem` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Embalagem` (
   `Nome_embalagem` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`Nome_embalagem`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Categoria_has_Produto`
+-- Table `db_alimentos`.`Categoria_has_Produto`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Categoria_has_Produto` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Categoria_has_Produto` (
   `Categoria_idCategoria` INT NOT NULL ,
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   PRIMARY KEY (`Categoria_idCategoria`, `Produto_Cod_barras`) ,
@@ -154,21 +156,21 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Categoria_has_Produto` (
   INDEX `fk_Categoria_has_Produto_Categoria1_idx` (`Categoria_idCategoria` ASC) ,
   CONSTRAINT `fk_Categoria_has_Produto_Categoria1`
     FOREIGN KEY (`Categoria_idCategoria` )
-    REFERENCES `db_ra165684`.`Categoria` (`idCategoria` )
+    REFERENCES `db_alimentos`.`Categoria` (`idCategoria` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Categoria_has_Produto_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Produto_has_Embalagem`
+-- Table `db_alimentos`.`Produto_has_Embalagem`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Embalagem` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Produto_has_Embalagem` (
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   `Embalagem_Nome_embalagem` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`Produto_Cod_barras`, `Embalagem_Nome_embalagem`) ,
@@ -176,62 +178,64 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Embalagem` (
   INDEX `fk_Produto_has_Embalagem_Produto1_idx` (`Produto_Cod_barras` ASC) ,
   CONSTRAINT `fk_Produto_has_Embalagem_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Produto_has_Embalagem_Embalagem1`
     FOREIGN KEY (`Embalagem_Nome_embalagem` )
-    REFERENCES `db_ra165684`.`Embalagem` (`Nome_embalagem` )
+    REFERENCES `db_alimentos`.`Embalagem` (`Nome_embalagem` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Premio`
+-- Table `db_alimentos`.`Premio`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Premio` (
-  `Nome_Premio` VARCHAR(200) NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Premio` (
+  `Nome_Premio` VARCHAR(200) NULL ,
   `Ano` INT NULL ,
-  PRIMARY KEY (`Nome_Premio`) )
+  `Id` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`Id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Produto_has_Premio`
+-- Table `db_alimentos`.`Produto_has_Premio`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Premio` (
-  `Premio_Nome_Premio` VARCHAR(200) NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Produto_has_Premio` (
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
-  PRIMARY KEY (`Premio_Nome_Premio`, `Produto_Cod_barras`) ,
+  `Id` INT NOT NULL ,
+  `Premio_Id` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`Produto_Cod_barras`, `Id`) ,
   INDEX `fk_Premio_has_Produto_Produto1_idx` (`Produto_Cod_barras` ASC) ,
-  INDEX `fk_Premio_has_Produto_Premio1_idx` (`Premio_Nome_Premio` ASC) ,
-  CONSTRAINT `fk_Premio_has_Produto_Premio1`
-    FOREIGN KEY (`Premio_Nome_Premio` )
-    REFERENCES `db_ra165684`.`Premio` (`Nome_Premio` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+  INDEX `fk_Produto_has_Premio_Premio1_idx` (`Premio_Id` ASC) ,
   CONSTRAINT `fk_Premio_has_Produto_Produto1`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Produto_has_Premio_Premio1`
+    FOREIGN KEY (`Premio_Id` )
+    REFERENCES `db_alimentos`.`Premio` (`Id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Caracteristica`
+-- Table `db_alimentos`.`Caracteristica`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Caracteristica` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Caracteristica` (
   `Nome_Caracteristica` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`Nome_Caracteristica`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_ra165684`.`Produto_has_Caracteristica`
+-- Table `db_alimentos`.`Produto_has_Caracteristica`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Caracteristica` (
+CREATE  TABLE IF NOT EXISTS `db_alimentos`.`Produto_has_Caracteristica` (
   `Caracteristica_Nome_Caracteristica` VARCHAR(100) NOT NULL ,
   `Produto_Cod_barras` VARCHAR(12) NOT NULL ,
   PRIMARY KEY (`Caracteristica_Nome_Caracteristica`, `Produto_Cod_barras`) ,
@@ -239,18 +243,20 @@ CREATE  TABLE IF NOT EXISTS `db_ra165684`.`Produto_has_Caracteristica` (
   INDEX `fk_Produto_has_Caracteristica_Produto1_idx` (`Produto_Cod_barras` ASC) ,
   CONSTRAINT `fk_Produto_has_Caracteristica_Prod`
     FOREIGN KEY (`Produto_Cod_barras` )
-    REFERENCES `db_ra165684`.`Produto` (`Cod_barras` )
+    REFERENCES `db_alimentos`.`Produto` (`Cod_barras` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Produto_has_Caracteristica_Carac`
     FOREIGN KEY (`Caracteristica_Nome_Caracteristica` )
-    REFERENCES `db_ra165684`.`Caracteristica` (`Nome_Caracteristica` )
+    REFERENCES `db_alimentos`.`Caracteristica` (`Nome_Caracteristica` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+USE `db_alimentos` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
---
+++ proj1/consultas.sql
@@ -0,0 +1,47 @@
+-- Retorna o nome do produto, o prêmio e o ano do prêmio em ordem alfabética de nome.
+-- Para cada nome, ordena os prêmios de acordo com os anos.
+-- !!!!!!!!!!!REFAZER!!!!!!!!!!
+SELECT Produto.Nome, Premio.Nome_Premio, Premio.Ano FROM Produto
+	JOIN Produto_has_Premio
+	ON Produto_has_Premio.Produto_Cod_barras = Produto.Cod_barras
+	JOIN Premio
+	ON Produto_has_Premio.Premio_Id = Premio.Id
+	ORDER BY Produto.Nome, Premio.Ano;
+
+
+
+
+-- Lista de países que compram mais caloria (em ordem decrescente da quantidade de caloria comprada)
+SELECT Paises_Vendidos.Pais_nomePais, SUM(Produto.Calorias) AS cal_total FROM Produto
+	JOIN Paises_Vendidos
+	ON Paises_Vendidos.Produto_Cod_barras = Produto.Cod_barras
+	GROUP BY Paises_Vendidos.Pais_nomePais
+	ORDER BY cal_total DESC;
+
+-- Resulta em uma tabela com os paises e a média da quantidade (peso) comprada por eles, ordenada de forma decrescente.
+SELECT AVG(Produto.Quantidade), Paises_Vendidos.Pais_nomePais
+FROM Produto
+INNER JOIN Paises_Vendidos
+	on Produto.Cod_barras = Paises_Vendidos.Produto_Cod_barras
+GROUP BY Paises_Vendidos.Pais_nomePais
+ORDER BY AVG(Produto.Quantidade) DESC;
+
+-- Para todo fabricante, dá o número de produtos que ele possui
+SELECT Fabricante.nome, COUNT(Produto.Cod_barras) as num_produto FROM Fabricante
+    LEFT OUTER JOIN Produto
+    ON Produto.IdFabricante = Fabricante.IdFabricante
+    GROUP BY Fabricante.IdFabricante;
+
+-- Dá o nome da categoria e o número de produtos com proteínas maior do que 20 para cada categoria.
+SELECT Count(Produto_p.Proteinas) AS Num_Produtos, Categoria.Nome as Categoria
+FROM (  SELECT 
+	Proteinas,
+	Cod_barras
+	FROM Produto
+	WHERE Proteinas >= 20
+	) Produto_p
+INNER JOIN Categoria_has_Produto
+	on Produto_p.Cod_barras = Categoria_has_Produto.Produto_Cod_barras
+	INNER JOIN Categoria
+		ON Categoria_has_Produto.Categoria_idCategoria = Categoria.idCategoria
+GROUP BY Categoria_has_Produto.Categoria_idCategoria;
